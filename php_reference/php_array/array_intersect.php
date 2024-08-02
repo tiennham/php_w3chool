7583b553dcf4f18838d5cc2047438c05b0b6e02f
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-08-02 09:04:23 +0700

    intersect: keep items that have the same value

--- php_reference/php_array/array_intersect.php
+++ php_reference/php_array/array_intersect.php
@@ -3,18 +3,23 @@
 /*
 
 Definition and Usage
-Return an arrays that collect the items has same * key *
-the value kept the first array
+Return an arrays that collect the items has same * value *
+
 */
 
-$vk_family = array("ma"=>"ma_vk","ba"=>"ba_vk","em"=>"em_vk","anh"=>"anh_vk");
-$ck_family = array("ma"=>"ma_ck","ba"=>"ba_ck","em"=>"em_ck", "chi"=>"chi_ck");
+$vk_family = array("ma"=>"ma_vk","ba"=>"ba_vk","em"=>"ba_co_kho_tinh","sui_gia"=>"anh_chi_sui");
+$ck_family = array("ma"=>"ma_ck","ba"=>"ba_ck","chi"=>"ba_co_kho_tinh","sui_gia"=>"anh_chi_sui");
 
 
 print_r(array_intersect($vk_family,$ck_family));
 
 /*
  * Result
+Array
+(
+    [em] => ba_co_kho_tinh
+    [sui_gia] => anh_chi_sui
+)
 
  * */
 
