995f6ae97a612cc96561c3ece5ad10aa52833781
tiennham <hongtiennham@gmail.com>
2024-08-02 11:05:03 +0700

    fill: create an array with default value in specific keys

---
+++ php_reference/php_array/array_fill.php
@@ -0,0 +1,22 @@
+<?php
+### array_fill ###
+/*
+
+Definition and Usage
+The array_fill() return an array that the items were filled with specific value
+*/
+$a1=array_fill(3,4,"blue");
+print_r($a1);
+
+/*
+ * Result
+Array
+(
+    [3] => blue
+    [4] => blue
+    [5] => blue
+    [6] => blue
+)
+
+ * */
+
