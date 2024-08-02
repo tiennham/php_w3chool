5d3e88ab65f07e0664036bd74ef157f6186fcc97
Tien Nham <tiennham@Tiens-MacBook-Pro.local>
2024-08-02 10:47:25 +0700

    intersect: the value of items that 'through' all participation array

--- php_reference/php_array/array_intersect.php
+++ php_reference/php_array/array_intersect.php
@@ -23,3 +23,16 @@ Array
 
  * */
 
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"black","g"=>"purple");
+$a3=array("a"=>"red","b"=>"black","h"=>"yellow");
+
+$result=array_intersect($a1,$a2,$a3);
+print_r($result);
+
+/*
+ * Result
+Array ( [a] => red )
+
+ * */
+
