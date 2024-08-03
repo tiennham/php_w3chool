64dc9d922a88c8ae4a6532de2335f8bccc328c36
tiennham <hongtiennham@gmail.com>
2024-08-03 18:12:00 +0700

    file: create, write, upload

---
+++ php_advanced/php_file_create_append.php
@@ -0,0 +1,7 @@
+<?php
+$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+$txt = "Donald Duck\n";
+fwrite($myfile, $txt);
+$txt = "Goofy Goof\n";
+fwrite($myfile, $txt);
+fclose($myfile);
